9d5bc42104085f99ec1b43dc341f16758cf70ebe
Sebastien Brisard <sebastien.brisard@ifsttar.fr>
2013-10-21 08:26:43 +0200

    Configuration of color themes.

---
+++ color-theme-setup.el
@@ -0,0 +1,10 @@
+;; +------------------------------+
+;; | Configuration of color theme |
+;; +------------------------------+
+;;
+;; M-x customize-themes displays a selectable list of custom themes.
+
+(setq custom-safe-themes (quote ("d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" default)))
+
+(load-theme 'solarized-light)
+

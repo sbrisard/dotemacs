1236cb990cd372853b2b2faa64e281e6870490e9
Sebastien Brisard <sebastien.brisard@ifsttar.fr>
2013-10-23 05:55:13 +0200

    Added commands to load AUCTeX.

--- auctex-setup.el
+++ auctex-setup.el
@@ -2,6 +2,9 @@
 ;; | Configuration of AUCTeX |
 ;; +-------------------------+
 
+(load "auctex.el" nil t t)
+(load "preview-latex.el" nil t t)
+
 ;; AUCTeX multifile
 (setq TeX-parse-self t) ; Enable parse on load.
 ;;(setq TeX-auto-save t) ; Enable parse on save.

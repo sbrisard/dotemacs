c6ead8d7ad8730cff38f5b88869027443f0bbeb6
Sebastien Brisard <sebastien.brisard@ifsttar.fr>
2014-01-15 10:36:09 +0100

    Check if variable sb-path-to-blog before setting up org-publish for this project.

--- org-mode-setup.el
+++ org-mode-setup.el
@@ -24,64 +24,66 @@
 (setq org-html-htmlize-output-type 'css)
 
 ;; org-publish configuration
-(defvar sb-path-to-blog-base-directory (concat sb-path-to-blog "org"))
-(defvar sb-path-to-blog-publishing-directory (concat sb-path-to-blog "html"))
+(when (boundp 'sb-path-to-blog)
+  (defvar sb-path-to-blog-base-directory (concat sb-path-to-blog "org"))
+  (defvar sb-path-to-blog-publishing-directory (concat sb-path-to-blog "html"))
 
-;; From http://lists.gnu.org/archive/html/emacs-orgmode/2008-11/msg00571.html
-;;
-;; Hi Richard,
-;;
-;; no, variables are not interpolated into quoted lists,
-;; any list preceded by "'" is quoted.
-;;
-;; If you can guarantee that the value of the variables is define at the time
-;; the
-;;
-;;   (setq org-publish-projects-alist ...
-;;
-;; is executed, then you can use backquote syntax: Quote the main list with the
-;; backquote, and then preceed any variable inside you would like to have
-;;evaluated with a comma so
-;;
-;; (setq org-publish-projects-alist
-;;        `( .............
-;;            ,rgr-souerce
-;;            ....))
-;;
-;; Note that this works only once, so if you later change the value, this
-;; list will not be changed.
-::
-;; If you wanted dynamic behavior, then we would have to patch org- publish.el.
-;;
-;; HTH
-;;
-;; - Carsten
-;;
-(setq org-publish-project-alist
-      `(("blog-orgfiles"
-         :base-directory ,sb-path-to-blog-base-directory
-         :publishing-directory ,sb-path-to-blog-publishing-directory
-         :base-extension "org"
-         :exclude "header.org"
-         :recursive t
-         :publishing-function org-html-publish-to-html
-         :section-numbers nil
-         :with-toc nil)
-        ("blog-images"
-         :base-directory ,sb-path-to-blog-base-directory
-         :publishing-directory ,sb-path-to-blog-publishing-directory
-         :base-extension "jpg\\|gif\\|png"
-         :recursive t
-         :publishing-function org-publish-attachment)
-        ("blog-other"
-         :base-directory ,sb-path-to-blog-base-directory
-         :publishing-directory ,sb-path-to-blog-publishing-directory
-         :base-extension "css"
-         :recursive t
-         :publishing-function org-publish-attachment)
-        ("blog"
-         :components ("blog-orgfiles" "blog-images" "blog-other"))))
-
+  ;; From http://lists.gnu.org/archive/html/emacs-orgmode/2008-11/msg00571.html
+  ;;
+  ;; Hi Richard,
+  ;;
+  ;; no, variables are not interpolated into quoted lists,
+  ;; any list preceded by "'" is quoted.
+  ;;
+  ;; If you can guarantee that the value of the variables is define at the
+  ;; time the
+  ;;
+  ;;   (setq org-publish-projects-alist ...
+  ;;
+  ;; is executed, then you can use backquote syntax: Quote the main list with
+  ;; the backquote, and then preceed any variable inside you would like to
+  ;; have evaluated with a comma so
+  ;;
+  ;; (setq org-publish-projects-alist
+  ;;        `( .............
+  ;;            ,rgr-souerce
+  ;;            ....))
+  ;;
+  ;; Note that this works only once, so if you later change the value, this
+  ;; list will not be changed.
+  ::
+  ;; If you wanted dynamic behavior, then we would have to patch
+  ;; org-publish.el.
+  ;;
+  ;; HTH
+  ;;
+  ;; - Carsten
+  ;;
+  (setq org-publish-project-alist
+        `(("blog-orgfiles"
+           :base-directory ,sb-path-to-blog-base-directory
+           :publishing-directory ,sb-path-to-blog-publishing-directory
+           :base-extension "org"
+           :exclude "header.org"
+           :recursive t
+           :publishing-function org-html-publish-to-html
+           :section-numbers nil
+           :with-toc nil)
+          ("blog-images"
+           :base-directory ,sb-path-to-blog-base-directory
+           :publishing-directory ,sb-path-to-blog-publishing-directory
+           :base-extension "jpg\\|gif\\|png"
+           :recursive t
+           :publishing-function org-publish-attachment)
+          ("blog-other"
+           :base-directory ,sb-path-to-blog-base-directory
+           :publishing-directory ,sb-path-to-blog-publishing-directory
+           :base-extension "css"
+           :recursive t
+           :publishing-function org-publish-attachment)
+          ("blog"
+           :components ("blog-orgfiles" "blog-images" "blog-other")))))
+  
 ;; This allows proper handling of links to "dedicated targets" accross multiple
 ;; files.
 ;; Let's say file1.og contains

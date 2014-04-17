fccadc6f91bd80a59b5ce3e8834f660345be4979
Sebastien Brisard <sebastien.brisard@ifsttar.fr>
2014-04-17 10:43:28 +0200

    Moved some settings from org-mode-setup.el to emacs-setup.el

--- emacs-setup.el
+++ emacs-setup.el
@@ -235,6 +235,13 @@
 (global-set-key "\C-cL" 'org-insert-link-global)  ;; Allow for links to be used
 (global-set-key "\C-co" 'org-open-at-point-global)               ;; outside Org
 
+;; Visit files in same window
+(setq org-link-frame-setup (quote ((vm . vm-visit-folder-other-frame)
+                                   (vm-imap . vm-visit-imap-folder-other-frame)
+                                   (gnus . org-gnus-no-new-news)
+                                   (file . find-file)
+                                   (wl . wl-other-frame))))
+
 ;; Python/Cython                                                     <<python>>
 ;; =============
 

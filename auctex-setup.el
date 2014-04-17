37425ec62e5b3b2428f9809beba6b07dc61edb2f
Sebastien Brisard <sebastien.brisard@ifsttar.fr>
2014-04-17 09:01:09 +0200

    Moved custom-set-variables related to LaTeX to auctex-setup.el. Configured Skim as default PDF viewer under MacOS.

--- auctex-setup.el
+++ auctex-setup.el
@@ -21,6 +21,23 @@
                                             ("citealt" "*[{")
                                             ("citealp" "*[{")))
 
+(setq LaTeX-command "latex")
+(setq-default TeX-PDF-mode t)
+(setq TeX-command "tex")
+(setq TeX-source-correlate-method (quote synctex))
+(setq TeX-source-correlate-mode t)
+(setq TeX-source-correlate-start-server t)
+
+(when windowsp
+  (setq TeX-view-program-list (quote (("SumatraPDF" ("\"C:/Program Files (x86)/SumatraPDF/SumatraPDF.exe\" -reuse-instance" (mode-io-correlate " -forward-search %b %n") " %o")))))
+  (setq TeX-view-program-selection (quote (((output-dvi style-pstricks) "dvips and gv") (output-dvi "xdvi") (output-pdf "SumatraPDF") (output-html "xdg-open")))))
+
+(when darwinp
+  (add-to-list 'TeX-expand-list '("%(skim)" (lambda () (format "/Applications/Skim.app/Contents/SharedSupport/displayline"))))
+  ;;(add-to-list 'TeX-view-program-list '("Skim" "%(skim) -r -b %(line-number) %(pdf-file-name) %(tex-file-name)"))
+  (add-to-list 'TeX-view-program-list '("Skim" "%(skim) -r -b %n %o %b"))
+  (setq TeX-view-program-selection '((output-pdf "Skim"))))
+
 ;; From the AUCTeX FAQ
 ;;     When writing the log file, TeX puts information related to a file,
 ;;     including error messages, between a pair of parentheses. AUCTeX
@@ -33,5 +50,5 @@
 ;;     Either you do this in the configuration of your TeX system (consult its
 ;;     manual to see where this is) or you add a command line switch to the
 ;;    (la)tex call, e.g. by customizing LaTeX-command-style or
-;;    TeX-command-list. 
+;;    TeX-command-list.
 (setq LaTeX-command-style '(("" "%(PDF)%(latex) -file-line-error %S%(PDFout)")))
--- emacs-setup.el
+++ emacs-setup.el
@@ -129,14 +129,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(LaTeX-command "latex")
- '(TeX-PDF-mode t)
- '(TeX-command "tex")
- '(TeX-source-correlate-method (quote synctex))
- '(TeX-source-correlate-mode t)
- '(TeX-source-correlate-start-server t)
- '(TeX-view-program-list (quote (("SumatraPDF" ("\"C:/Program Files (x86)/SumatraPDF/SumatraPDF.exe\" -reuse-instance" (mode-io-correlate " -forward-search %b %n") " %o")))))
- '(TeX-view-program-selection (quote (((output-dvi style-pstricks) "dvips and gv") (output-dvi "xdvi") (output-pdf "SumatraPDF") (output-html "xdg-open"))))
  '(column-number-mode t)
  '(display-buffer-reuse-frames t)
 ; '(font-use-system-font t)

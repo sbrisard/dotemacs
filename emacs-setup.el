8f520fc9f28b4b8b75fd0c7d121f2be8b6454fe2
Sebastien Brisard <sebastien.brisard@ifsttar.fr>
2014-04-25 09:28:38 +0200

    New subsection 'ELPA'.

--- emacs-setup.el
+++ emacs-setup.el
@@ -94,6 +94,17 @@
 (setq-default indicate-empty-lines t)
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
 
+;; Emacs Lisp Package Archive (ELPA)
+;; ---------------------------------
+
+(require 'package)
+
+(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
+                         ("marmalade" . "http://marmalade-repo.org/packages/")
+                         ("melpa" . "http://melpa.milkbox.net/packages/")))
+
+(package-initialize)
+
 ;; Platform-specific customizations
 ;; --------------------------------
 

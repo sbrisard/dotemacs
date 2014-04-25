1ff9e3255848b94295df7449c34eccaad2fb2aa3
Sebastien Brisard <sebastien.brisard@ifsttar.fr>
2014-04-25 10:14:04 +0200

    Removed unused files.

--- init.el
+++
@@ -1,6 +0,0 @@
-;; This is an example of init.el file.
-;; Save a copy of this file in your .emacs.d directory. 
-(defvar sb-path-to-emacs-setup "~/Documents/emacs-setup/")
-
-(load-file (concat sb-path-to-emacs-setup "emacs-setup.el"))
-

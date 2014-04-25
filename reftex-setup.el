9aa4b873ff4f23b9388b73262e47a58bfe1b7a93
Sebastien Brisard <sebastien.brisard@ifsttar.fr>
2014-04-25 09:40:46 +0200

    New section 'LaTeX'.

--- reftex-setup.el
+++ reftex-setup.el
@@ -1,21 +1,3 @@
 ;; +-------------------------+
 ;; | Configuration of RefTeX |
 ;; +-------------------------+
-
-(require 'reftex)
-
-;; Turn on RefTeX in emacs latex mode
-(add-hook 'latex-mode-hook 'turn-on-reftex)
-
-;; Turn on RefTeX in AUCTeX LaTeX mode
-(add-hook 'LaTeX-mode-hook 'turn-on-reftex)
-
-;; Add the buffer's index to the menu bar in latex mode
-(setq reftex-load-hook (quote (imenu-add-menubar-index)))
-(setq reftex-mode-hook (quote (imenu-add-menubar-index)))
-
-;; Turn on the RefTeX-AUCTeX interface. When on, both packages interact
-;; closely.
-(setq reftex-plug-into-AUCTeX t)
-
-(setq reftex-insert-label-flags (quote ("s" "st")))

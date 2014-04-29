ef903fe275f521e9608e35ae56b1187d2d858d7c
Sebastien Brisard <sebastien.brisard@ifsttar.fr>
2014-04-29 20:57:24 +0200

    Typo: missing closing ')'.

--- init.el
+++ init.el
@@ -31,7 +31,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(scroll-bar-mode nil)
+ '(scroll-bar-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.

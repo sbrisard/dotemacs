a5c2981bdf9b5ed0cb43b9cea11f5523123cbce5
Sebastien Brisard <sebastien.brisard@ifsttar.fr>
2014-05-24 14:13:15 +0200

    Moved global-auto-revert-mode outside custom-set-variables.

--- init.el
+++ init.el
@@ -35,7 +35,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(global-auto-revert-mode t)
  '(make-backup-files nil)
  '(safe-local-variable-values (quote ((buffer-auto-save-file-name))))
  '(scroll-bar-mode nil))
@@ -57,6 +56,7 @@
 (setq default-directory "~/" )
 (setq dnd-open-file-other-window nil)
 (setq find-file-run-dired nil)
+(setq global-auto-revert-mode t)
 (setq inhibit-startup-screen t)
 (setq ps-paper-type (quote a4))
 (setq visible-bell t)

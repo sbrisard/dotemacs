04cc76da019b751e8c71cb50ba05523cf00e7985
Sebastien Brisard <sebastien.brisard@ifsttar.fr>
2014-05-24 14:20:46 +0200

    Moved make-backup-files outside custom-set-variables.

--- init.el
+++ init.el
@@ -35,7 +35,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(make-backup-files nil)
  '(safe-local-variable-values (quote ((buffer-auto-save-file-name))))
  '(scroll-bar-mode nil))
 (custom-set-faces
@@ -56,12 +55,13 @@
 (setq default-directory "~/" )
 (setq dnd-open-file-other-window nil)
 (setq find-file-run-dired nil)
-(setq global-auto-revert-mode t)
 (setq inhibit-startup-screen t)
+(setq make-backup-files nil)
 (setq ps-paper-type (quote a4))
 (setq visible-bell t)
 
 (filesets-init)
+(global-auto-revert-mode)
 (global-hl-line-mode)
 (menu-bar-mode)
 ;(scroll-bar-mode -1)

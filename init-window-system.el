5da297514bb3d7539cd40c803ece07079147f40f
Sebastien Brisard <sebastien.brisard@ifsttar.fr>
2014-07-14 20:35:54 +0200

    Switched to the Zenburn theme by Bozhidar Batsov.

--- init-window-system.el
+++ init-window-system.el
@@ -50,13 +50,14 @@
 
 ;; Do not use variable height fonts. These variables must be set prior to
 ;; loading the theme itself, for them to take effect.
-(setq solarized-height-minus-1 1.0)
-(setq solarized-height-plus-1 1.0)
-(setq solarized-height-plus-2 1.0)
-(setq solarized-height-plus-3 1.0)
-(setq solarized-height-plus-4 1.0)
+;(setq solarized-height-minus-1 1.0)
+;(setq solarized-height-plus-1 1.0)
+;(setq solarized-height-plus-2 1.0)
+;(setq solarized-height-plus-3 1.0)
+;(setq solarized-height-plus-4 1.0)
 
-(load-theme 'solarized-light t)
+;(load-theme 'solarized-light t)
+(load-theme 'zenburn t)
 
 ;; Fonts
 ;; -----

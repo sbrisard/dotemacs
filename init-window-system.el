19e174299f57d50d12221d01a28fc505ca339d5d
Sebastien Brisard <sebastien.brisard@ifsttar.fr>
2014-08-05 09:57:44 +0200

    Improved configuration of IPython shell.

--- init-window-system.el
+++ init-window-system.el
@@ -349,13 +349,15 @@
 ;; Python shell
 ;; ------------
 
+;; Code copied from python.el
 (setq python-shell-interpreter "ipython")
 (setq python-shell-interpreter-args "")
-
+(setq python-shell-prompt-regexp "In \\[[0-9]+\\]: ")
+(setq python-shell-prompt-output-regexp "Out\\[[0-9]+\\]: ")
 (setq python-shell-completion-setup-code
       "from IPython.core.completerlib import module_completion")
-(setq  python-shell-completion-module-string-code
-       "';'.join(module_completion('''%s'''))\n")
+(setq python-shell-completion-module-string-code
+      "';'.join(module_completion('''%s'''))\n")
 (setq python-shell-completion-string-code
       "';'.join(get_ipython().Completer.all_completions('''%s'''))\n")
 

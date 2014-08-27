93e71b099aee4808dbb20f91d7cd59da8623e9c5
Sebastien Brisard <sebastien.brisard@ifsttar.fr>
2014-08-27 20:42:58 +0200

    Get rid of ido.

--- init.el
+++ init.el
@@ -121,12 +121,6 @@
 (setq filesets-menu-path '("File"))
 (filesets-install t)
 
-;; Interactively Do Things (ido)                                        <<ido>>
-;; =============================
-
-(require 'ido)
-(ido-mode t)
-
 ;; Whitespace mode (minor mode to visualize blanks)              <<whitespace>>
 ;; ================================================
 

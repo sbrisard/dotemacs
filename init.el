54f21dad25f2de55d5251b6a68f5e33752ae2d94
Sebastien Brisard <sebastien.brisard@ifsttar.fr>
2014-11-19 07:59:11 +0100

    Filesets no longer used.

--- init.el
+++ init.el
@@ -12,7 +12,6 @@
 ;; [[trailing][Trailing whitespaces]]
 ;; [[elpa][Emacs Lisp Package Archive (ELPA)]]
 ;; [[epa][Easy PG (GnuPG interface for Emacs)]]
-;; [[filesets][Filesets]]
 ;; [[whitespace][Whitespace mode (minor mode to visualize blanks)]]
 ;; [[window][Additional customizations in window-system mode]]
 
@@ -114,18 +113,6 @@
 
 (require 'epa-file)
 (epa-file-enable)
-
-;; Filesets                                                        <<filesets>>
-;; ========
-
-;; The version of filesets which ships with emacs seems buggy. It is preferable
-;; to download the latest version by the author
-;;   M-x filesets-goto-homepage
-
-(add-to-list 'load-path (concat user-emacs-directory "elisp/filesets2"))
-(require 'filesets-emacs)
-(setq filesets-menu-path '("File"))
-(filesets-install t)
 
 ;; Whitespace mode (minor mode to visualize blanks)              <<whitespace>>
 ;; ================================================

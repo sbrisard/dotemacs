36514c1d05e52e8153651b29717dfc94c89093fc
Sébastien Brisard <sebastien.brisard@m4x.org>
2015-08-18 08:38:39 +0200

    Moved custom section to init.el.

--- sb-init.el
+++ sb-init.el
@@ -60,22 +60,6 @@
   "Bind newline-and-indent to RET. This function can be used as a hook."
   (local-set-key (kbd "RET") 'newline-and-indent))
 
-;; Custom-set variables and faces                                    <<custom>>
-;; ==============================
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(scroll-bar-mode nil))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-
 ;; Miscellaneous                                                       <<misc>>
 ;; =============
 

836173df08e1f76f949e2d2a0aa6beafd609edd4
Sébastien Brisard <sebastien.brisard@m4x.org>
2015-08-18 08:11:01 +0200

    Changed default package repos.

--- init.el
+++ init.el
@@ -22,8 +22,8 @@
 (require 'package)
 
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
-                         ("marmalade" . "http://marmalade-repo.org/packages/")
-                         ("melpa" . "http://melpa.milkbox.net/packages/")))
+                         ("melpa" . "http://melpa.org/packages/")
+                         ("melpa-stable" . "http://stable.melpa.org/packages/")))
 
 (package-initialize)
 

b3dc4cb923dd7de14af0f073cffad29e393ae571
Sébastien Brisard <sebastien.brisard@m4x.org>
2018-03-02 07:35:24 +0100

    smartparens: initialization

---
+++ sp-init.el
@@ -0,0 +1,23 @@
+(use-package smartparens
+  :init
+  (show-paren-mode -1))
+
+(require 'smartparens-config)
+(require 'smartparens-latex)
+(smartparens-global-mode t)
+(show-smartparens-global-mode t)
+
+(bind-key "C-M-f" #'sp-forward-sexp)
+(bind-key "C-M-b" #'sp-backward-sexp)
+(bind-key "C-M-k" #'sp-kill-sexp)
+(bind-key "C-M-t" #'sp-transpose-sexp)
+(bind-key "C-M-<SPC>" #'sp-mark-sexp)
+(bind-key "C-M-n" #'sp-forward-hybrid-sexp)
+(bind-key "C-M-p" #'sp-backward-hybrid-sexp)
+(bind-key "C-M-u" #'sp-backward-up-sexp)
+(bind-key "C-M-d" #'sp-down-sexp)
+
+(defun sb-get-enclosing-sexp-as-string (&optional arg)
+  (interactive)
+  (let ((enc (sp-get-enclosing-sexp)))
+    (buffer-substring (sp-get enc :beg) (sp-get enc :end))))

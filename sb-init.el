92295018d6bfcb70b1c3f974f62c85fb0676dd0b
Sébastien Brisard <sebastien.brisard@m4x.org>
2019-03-18 10:57:14 +0100

    RefTeX: derive label from context and prompt for confirmation

--- sb-init.el
+++ sb-init.el
@@ -428,7 +428,7 @@ This function uses magit only to display the current status."
   (setq reftex-load-hook (quote (imenu-add-menubar-index))
 	reftex-mode-hook (quote (imenu-add-menubar-index))
 	reftex-plug-into-AUCTeX t
-	reftex-insert-label-flags (quote (nil nil))
+	reftex-insert-label-flags (quote (t t))
 	reftex-ref-macro-prompt nil
 	reftex-label-alist
 	'(("axiom"   ?a "ax:"  "~\\ref{%s}" nil ("axiom"   "ax.") -2)

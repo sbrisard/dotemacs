5dd1a214ad2379aa8f4d8553de8123ae3b5d486e
Sebastien Brisard <sebastien.brisard@m4x.org>
2019-08-13 05:30:39 +0200

    Javascript support

--- sb-init.el
+++ sb-init.el
@@ -62,6 +62,8 @@
     (sb-package-install-unless-installed 'elpy)
     (sb-package-install-unless-installed 'htmlize)
     (sb-package-install-unless-installed 'ivy)
+    (sb-package-install-unless-installed 'js2-mode)
+    (sb-package-install-unless-installed 'js2-refactor)
     (sb-package-install-unless-installed 'lsp-mode)
     (sb-package-install-unless-installed 'lsp-python-ms)
     (sb-package-install-unless-installed 'lsp-ui)
@@ -70,7 +72,8 @@
     (sb-package-install-unless-installed 'meson-mode)
     (sb-package-install-unless-installed 'ob-ipython)
     (sb-package-install-unless-installed 'spaceline)
-    (sb-package-install-unless-installed 'spacemacs-theme)))
+    (sb-package-install-unless-installed 'spacemacs-theme)
+    (sb-package-install-unless-installed 'xref-js2)))
 
 (sb-init-package)
 
@@ -527,6 +530,28 @@ directory where the current buffer lives, or one of its parents."
   (setq python-flymake-command (quote ("flake8" "-"))))
 
 (sb-init-python)
+
+(defun sb-init-javascript ()
+  (require 'js2-mode)
+  (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+  (add-hook 'js2-mode-hook #'js2-imenu-extras-mode)
+
+  (require 'js2-refactor)
+  (require 'xref-js2)
+
+  (add-hook 'js2-mode-hook #'js2-refactor-mode)
+  (js2r-add-keybindings-with-prefix "C-c C-r")
+  (define-key js2-mode-map (kbd "C-k") #'js2r-kill)
+
+  ;; js-mode (which js2 is based on) binds "M-." which conflicts with xref, so
+  ;; unbind it.
+  (define-key js-mode-map (kbd "M-.") nil)
+
+  (add-hook 'js2-mode-hook
+	    (lambda () (add-hook 'xref-backend-functions
+				 #'xref-js2-xref-backend nil t))))
+
+(sb-init-javascript)
 
 (defun sb-init-lsp ()
   (require 'lsp-mode)

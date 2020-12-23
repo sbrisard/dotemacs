a646264bdf68d13a52f0e18ea47dfb8255fff2e1
Sebastien Brisard <sebastien.brisard@m4x.org>
2020-12-23 16:17:37 +0100

    Deprecate javascript-related code

--- deprecated.el
+++ deprecated.el
@@ -77,3 +77,25 @@ This function uses magit only to display the current status."
   (setenv "GIT_ASKPASS" "git-gui--askpass"))
 
 (sb-init-magit)
+
+(defun sb-init-javascript ()
+  (require 'js2-mode)
+  (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+  (add-hook 'js2-mode-hook #'js2-imenu-extras-mode)
+
+  (require 'js2-refactor)
+  (require 'xref-js2)
+
+  (add-hook 'js2-mode-hook #'js2-refactor-mode)
+  (js2r-add-keybindings-with-prefix "C-c C-r")
+  (define-key js2-mode-map (kbd "C-k") #'js2r-kill)
+
+  ;; js-mode (which js2 is based on) binds "M-." which conflicts with xref, so
+  ;; unbind it.
+  (define-key js-mode-map (kbd "M-.") nil)
+
+  (add-hook 'js2-mode-hook
+	    (lambda () (add-hook 'xref-backend-functions
+				 #'xref-js2-xref-backend nil t))))
+
+(sb-init-javascript)
--- sb-init.el
+++ sb-init.el
@@ -66,14 +66,11 @@
     (sb-package-install-unless-installed 'counsel)
     (sb-package-install-unless-installed 'elpy)
     (sb-package-install-unless-installed 'ivy)
-    (sb-package-install-unless-installed 'js2-mode)
-    (sb-package-install-unless-installed 'js2-refactor)
     (sb-package-install-unless-installed 'lsp-mode)
     (sb-package-install-unless-installed 'lsp-ui)
     (sb-package-install-unless-installed 'markdown-mode)
     (sb-package-install-unless-installed 'spaceline)
-    (sb-package-install-unless-installed 'spacemacs-theme)
-    (sb-package-install-unless-installed 'xref-js2)))
+    (sb-package-install-unless-installed 'spacemacs-theme)))
 
 (sb-init-package)
 
@@ -384,28 +381,6 @@ directory where the current buffer lives, or one of its parents."
   (setq python-flymake-command (quote ("flake8" "-"))))
 
 (sb-init-python)
-
-(defun sb-init-javascript ()
-  (require 'js2-mode)
-  (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
-  (add-hook 'js2-mode-hook #'js2-imenu-extras-mode)
-
-  (require 'js2-refactor)
-  (require 'xref-js2)
-
-  (add-hook 'js2-mode-hook #'js2-refactor-mode)
-  (js2r-add-keybindings-with-prefix "C-c C-r")
-  (define-key js2-mode-map (kbd "C-k") #'js2r-kill)
-
-  ;; js-mode (which js2 is based on) binds "M-." which conflicts with xref, so
-  ;; unbind it.
-  (define-key js-mode-map (kbd "M-.") nil)
-
-  (add-hook 'js2-mode-hook
-	    (lambda () (add-hook 'xref-backend-functions
-				 #'xref-js2-xref-backend nil t))))
-
-(sb-init-javascript)
 
 (defun sb-init-lsp ()
   (require 'lsp-mode)
